ebe93be49b11a1c662167fe5af262e5f1dd7972e
LIENLIN <lienlin@huatu.com>
2019-03-20 11:53:51 +0800

    临时提交

--- module_employees/src/main/java/com/example/module_employees_world/EmplayeesActivity.java
+++ module_employees/src/main/java/com/example/module_employees_world/EmplayeesActivity.java
@@ -1,16 +1,40 @@
 package com.example.module_employees_world;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.wb.baselib.base.activity.MvpActivity;
+import com.wb.baselib.base.mvp.BasePreaenter;
 
 /**
  * 员工天地首页
  */
-public class EmplayeesActivity extends AppCompatActivity {
+public class EmplayeesActivity extends MvpActivity {
+
+    @Override
+    protected BasePreaenter onCreatePresenter() {
+        return null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emplayees_layout);
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void processLogic(Bundle savedInstanceState) {
+
     }
 }
