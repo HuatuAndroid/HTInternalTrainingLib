8510ec710412aaf90dbcb9b8909b42112502fa00
wangxiaofeng <wangxiaofeng@huatu.com>
2019-04-02 14:49:27 +0800

    picture preview

---
+++ module_employees/src/main/java/com/example/module_employees_world/adapter/MyImageAdapter.java
@@ -0,0 +1,63 @@
+package com.example.module_employees_world.adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.baijiayun.playback.ppt.photoview.PhotoView;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * 创建人：王晓凤
+ * 创建时间：2019/4/2 2:21 PM
+ */
+public class MyImageAdapter extends PagerAdapter {
+    public static final String TAG = MyImageAdapter.class.getSimpleName();
+    private List<String> imageUrls;
+    private AppCompatActivity activity;
+
+    public MyImageAdapter(List<String> imageUrls, AppCompatActivity activity) {
+        this.imageUrls = imageUrls;
+        this.activity = activity;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        String url = imageUrls.get(position);
+        PhotoView photoView = new PhotoView(activity);
+        Picasso.with(activity)
+                .load(url)
+                .into(photoView);
+        container.addView(photoView);
+        photoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // activity.finish();
+            }
+        });
+        return photoView;
+    }
+
+    @Override
+    public int getCount() {
+        return imageUrls != null ? imageUrls.size() : 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
+}
