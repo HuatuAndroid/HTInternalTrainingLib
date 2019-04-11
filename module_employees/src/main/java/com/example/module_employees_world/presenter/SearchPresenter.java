cdc0039512d3ef0c5ee67e2c518fe58092023d0d
wangxiaofeng <wangxiaofeng@huatu.com>
2019-04-11 16:12:59 +0800

    搜索回退2

--- module_employees/src/main/java/com/example/module_employees_world/presenter/SearchPresenter.java
+++ module_employees/src/main/java/com/example/module_employees_world/presenter/SearchPresenter.java
@@ -70,12 +70,7 @@ public class SearchPresenter extends SearchContranct.SearchPresenter {
 
             @Override
             public void onFail(ApiException e) {
-                if (page == 1) {
-                    mView.ErrorData();
-                } else {
-                    mView.showErrorMsg(e.getMessage());
-                    mView.isLoadMore(true);
-                }
+
             }
         });
     }
@@ -125,12 +120,7 @@ public class SearchPresenter extends SearchContranct.SearchPresenter {
 
             @Override
             public void onFail(ApiException e) {
-                if (page == 1) {
-                    mView.ErrorData();
-                } else {
-                    mView.showErrorMsg(e.getMessage());
-                    mView.isLoadMore(true);
-                }
+               
             }
         });
 
