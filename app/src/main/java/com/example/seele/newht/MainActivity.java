16d624bcc7a58c8eccf57c9e3496a8d536812186
liuzhe <zhe.liu@sistalk.com>
2019-04-12 22:09:26 +0800

    1，修改 广告页倒计时

--- app/src/main/java/com/example/seele/newht/MainActivity.java
+++ app/src/main/java/com/example/seele/newht/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_nmain);
         mlist=this.findViewById(R.id.mlist);
 //        RichText.initCacheDir(this);
 //        RichText.debugMode = true;
